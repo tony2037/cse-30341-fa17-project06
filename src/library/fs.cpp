fc37146ede463be984c63cf6d423be98e6b72f01
ztex <ztex030640417@gmail.com>
2020-07-15 15:19:29 +0800

    Fix Fix every bugs that it passed the tests

--- src/library/fs.cpp
+++ src/library/fs.cpp
@@ -41,6 +41,7 @@ void FileSystem::debug(Disk *disk) {
                 printf("    size: %u bytes\n", InodeBlock.Inodes[j].Size);
 
                 std::vector<uint32_t> directBlocks;
+                std::vector<uint32_t> indirectDataBlocks;
                 for (size_t k = 0; k < POINTERS_PER_INODE; k++) {
                     if (InodeBlock.Inodes[j].Direct[k] != 0) {
                         directBlocks.push_back(InodeBlock.Inodes[j].Direct[k]);
@@ -49,6 +50,19 @@ void FileSystem::debug(Disk *disk) {
                 if (!directBlocks.empty()) {
                     printf("    direct blocks:");
                     for (uint32_t directBlock : directBlocks) {printf(" %u", directBlock);}
+                    printf("\n");
+                }
+                if (InodeBlock.Inodes[j].Indirect != 0) {
+                    printf("    indirect block: %u\n", InodeBlock.Inodes[j].Indirect);
+                    Block indirectDataBlock;
+                    disk->read(InodeBlock.Inodes[j].Indirect, indirectDataBlock.Data);
+                    for (uint32_t k = 0; k < FileSystem::POINTERS_PER_BLOCK; k++) {
+                        if(indirectDataBlock.Pointers[k] != 0) {indirectDataBlocks.push_back(indirectDataBlock.Pointers[k]);}
+                    }
+                }
+                if (!indirectDataBlocks.empty()) {
+                    printf("    indirect data blocks:");
+                    for (uint32_t indirectDataBlock : indirectDataBlocks) {printf(" %u", indirectDataBlock);}
                     printf("\n");
                 }
             }
